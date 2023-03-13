8a07c051834db1f81f959490bcfcd92b6dcade74
dy k <37325163+sododuk32@users.noreply.github.com>
2023-03-13 19:37:39 +0900

    dd

--- src/app/component/crousel/crousel.component.ts
+++ src/app/component/crousel/crousel.component.ts
@@ -84,32 +84,5 @@ export class CrouselComponent {
   pauseOnFocus = true;
 
   @ViewChild('carousel', { static: true })
-  carousel!: NgbCarousel;
-
-  togglePaused() {
-    if (this.paused) {
-      this.carousel.cycle();
-    } else {
-      this.carousel.pause();
-    }
-    this.paused = !this.paused;
-  }
-
-  onSlide(slideEvent: NgbSlideEvent) {
-    if (
-      this.unpauseOnArrow &&
-      slideEvent.paused &&
-      (slideEvent.source === NgbSlideEventSource.ARROW_LEFT ||
-        slideEvent.source === NgbSlideEventSource.ARROW_RIGHT)
-    ) {
-      this.togglePaused();
-    }
-    if (
-      this.pauseOnIndicator &&
-      !slideEvent.paused &&
-      slideEvent.source === NgbSlideEventSource.INDICATOR
-    ) {
-      this.togglePaused();
-    }
-  }
+  staticCarousel!: NgbCarousel;
 }
